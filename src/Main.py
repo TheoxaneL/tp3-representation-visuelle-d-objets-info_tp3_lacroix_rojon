e22ead9ffa8ac35a76ff9152abd83c111171a635
TheoxaneL <90677510+TheoxaneL@users.noreply.github.com>
2021-12-09 12:02:40 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -16,13 +16,13 @@ import copy
 
 
 def Q1a():
-    pass
+    return Configuration()
     
 def Q1b_f():
-    return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}). \
-        setParameter('xAxisColor', [1, 1, 0]). \
-        setParameter('yAxisCo lor', [0,1,1]). \
-        display()
+    return Configuration({'screenPosition': -5, 'xAxisColor': [1, 1, 0]}).\
+    setParameter('xAxisColor', [1, 1, 0]).\
+    setParameter('yAxisCo lor', [0,1,1]). \
+    display()
         
 def Q2b():
     # Ecriture en utilisant le chaînage
